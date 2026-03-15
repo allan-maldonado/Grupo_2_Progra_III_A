75d9197a7e9f81b34e830f9512cd6a802a37411d
allan-maldonado <amaldonadoe3@miumg.edu.gt>
2026-03-14 21:35:56 -0600

    inicial

---
+++ grupo_2_progra_III_A/src/main/java/gt/edu/miumg/grupo_2_progra_iii_a/Grupo_2_progra_III_A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.grupo_2_progra_iii_a;
+
+/**
+ *
+ * @author Tecno
+ */
+public class Grupo_2_progra_III_A {
+
+    public static void main(String[] args) {
+        System.out.println("Hola buenas");
+    }
+}
