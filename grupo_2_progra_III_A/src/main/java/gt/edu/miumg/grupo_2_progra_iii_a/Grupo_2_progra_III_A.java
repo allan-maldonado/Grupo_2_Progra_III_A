5f0ba7e5c3a38c90f6fef7a57764b680d9f51d52
allan-maldonado <amaldonadoe3@miumg.edu.gt>
2026-03-23 22:27:49 -0600

    actualizar main
    Actualizacion de main

--- grupo_2_progra_III_A/src/main/java/gt/edu/miumg/grupo_2_progra_iii_a/Grupo_2_progra_III_A.java
+++ grupo_2_progra_III_A/src/main/java/gt/edu/miumg/grupo_2_progra_iii_a/Grupo_2_progra_III_A.java
@@ -10,7 +10,32 @@ package gt.edu.miumg.grupo_2_progra_iii_a;
  */
 public class Grupo_2_progra_III_A {
 
-    public static void main(String[] args) {
-        System.out.println("Hola buenas");
+   public static void main(String[] args) {
+        HojaCalculo miHoja = new HojaCalculo();
+        Evaluador motor = new Evaluador(miHoja);
+
+        //Inserción de datos
+        miHoja.insertar(1, 1, "50");   
+        miHoja.insertar(2, 1, "100");  
+        miHoja.insertar(3, 1, "200");  
+        miHoja.insertar(1, 2, "10");
+        miHoja.insertar(5, 5, "8"); 
+        miHoja.insertar(5, 10, "8"); 
+
+        // Pruebas de cálculo
+        System.out.println("--- PRUEBAS DE CALCULO ---");
+        System.out.println("Suma Simple (A1+A2): " + motor.procesar("=A1+A2"));
+        System.out.println("Rango (SUMAR(A1:B4)): " + motor.procesar("=SUMAR(A1:B4)"));
+        System.out.println("Multiplicacion Rango: " + motor.procesar("=MULTIPLICAR(A1:B1)"));
+
+        // 3. Modificación y Visualización
+        System.out.println("\n--- ESTADO DE LA HOJA ---");
+        Visualizador.mostrar(miHoja, 10, 10);
+
+        // 4. Eliminación
+        System.out.println("\nEliminando A2...");
+        miHoja.eliminar(2, 1);
+        Visualizador.mostrar(miHoja, 10, 10);
+        System.out.println("Nueva suma A1:B4: " + motor.procesar("=SUMAR(A1:B4)"));
     }
 }
